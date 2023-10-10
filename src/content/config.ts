897b50efb76e0adf18911225ad6ba6e75c9b008d
danielP <daniel.horsens@gmail.com>
2023-10-10 13:01:40 +0200

    Started on frontpage (missing dynamic))

---
+++ src/content/config.ts
@@ -0,0 +1,51 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z.coerce.date(),
+		updatedDate: z.coerce.date().optional(),
+		heroImage: z.string().optional(),
+	}),
+});
+
+const komponent = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		heroImage: z.string().optional(),	
+	})
+})
+
+const guide = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		heroImage: z.string().optional(),	
+	})
+})
+
+const tjeklist = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		heroImage: z.string().optional(),	
+	})
+})
+
+const top = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		pubDate: z.coerce.date(),
+		heroImage: z.string().optional(),	
+	})
+})
+
+export const collections = { blog, komponent , guide, tjeklist, top };
