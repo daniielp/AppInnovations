f72aa31b5900ec9edd8bdc7053039e32379144aa
danielP <daniel.horsens@gmail.com>
2023-10-17 09:15:05 +0200

    Fixed pageheader + created articles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		container: {
+			center: true,
+		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
 }
