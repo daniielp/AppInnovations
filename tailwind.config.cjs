022bd5edc50c35c150c0f880aa5b3ac722801eb4
danielP <daniel.horsens@gmail.com>
2023-10-18 21:08:14 +0200

    Added font to project

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -5,7 +7,11 @@ module.exports = {
 		container: {
 			center: true,
 		},
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans]
+			}
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
